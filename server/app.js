7d7c42767d6e84d0107ad7b0f03b1b8a935624f2
Jay patel <77409923+jaypatel345@users.noreply.github.com>
2026-05-07 08:39:18 +0530

    test: break CI pipeline

--- server/app.js
+++ server/app.js
@@ -12,8 +12,11 @@ import chatRoutes from "./src/routes/chat.routes.js";
 import cors from "cors";
 import requestLogger from "./src/middlewares/requestLogger.js";
 import testRoutes from "./src/routes/test.route.js";
+import ApiError from "./src/utils/ApiError.js";
 
 const app = express();
+
+throw new ApiError(500,"CI BREAK TEST");
 
 loadEnv();
 
