ca40875867c9fe74e6cdfe7983d8373356d0eff9
Jay patel <77409923+jaypatel345@users.noreply.github.com>
2026-06-28 18:30:56 +0530

    Remove .next from Git tracking

--- client/.next/dev/server/chunks/ssr/_next-internal_server_app_Enhancer_page_actions_90641b92.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/Enhancer/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_Enhancer_page_actions_90641b92.js.map
--- client/.next/dev/server/chunks/ssr/_next-internal_server_app_Teams_page_actions_476df0c4.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/Teams/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_Teams_page_actions_476df0c4.js.map
